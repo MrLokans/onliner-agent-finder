cb82db61f7f069bf2120cb3d2b55c4b67e15811e
MrLokans <trikster1911@gmail.com>
2017-04-24 19:41:42 +0300

    Added scrapy config

---
+++ config.py
@@ -0,0 +1,12 @@
+URL_FILE = 'apartment_urls.txt'
+
+SEARCH_BASE_URL = 'https://ak.api.onliner.by/search/apartments'
+LAT_INDEX, LONG_INDEX = 0, 1
+RENT_TYPE = '2_rooms'
+
+MINSK_BOUND_COORDINTATES = {
+    # Left bottom corner
+    'lb': ('53.78873227020693', '27.25622458457113'),
+    # Right top corner
+    'rt': ('54.01559842227507', '27.86870368988288')
+}
